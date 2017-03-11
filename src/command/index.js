956b6eaf671731dc446ad97adb45fce00435966f
Arman Yeghiazaryan <otanim617@gmail.com>
2017-03-11 19:14:12 +0400

    es5 fix

--- src/command/index.js
+++ src/command/index.js
@@ -28,7 +28,7 @@ const MATH = require('./math');
 exports = Object.assign(exports, MATH);
 
 //conditions regular expressions
-exports.IF_WHILE_REPEAT = `${exports.IF}|${exports.WHILE}|${exports.REPEAT}`;
-exports.CONDITION_VALUE = `(${exports.IF_WHILE_REPEAT})\\s+([^\\r\\n]*[^\\${exports.THEN}])( ${exports.THEN})*`;
-exports.CONDITION_OF_REPEAT_TIMES_VALUE = `(.*)\\s+${exports.TIMES}`;
-exports.CONDITION_TYPE = `\\s*(${exports.IF_WHILE_REPEAT}|${exports.ELSE}|${exports.DO}|${exports.ELIF})`;
+exports.IF_WHILE_REPEAT = exports.IF + "|" + exports.WHILE + "|" + exports.REPEAT;
+exports.CONDITION_VALUE = "(" + exports.IF_WHILE_REPEAT + ")\\s+([^\\r\\n]*[^\\" + exports.THEN + "])( " + exports.THEN + ")*";
+exports.CONDITION_OF_REPEAT_TIMES_VALUE = "(.*)\\s+" + exports.TIMES;
+exports.CONDITION_TYPE = "\\s*(" + exports.IF_WHILE_REPEAT + "|" + exports.ELSE + "|" + exports.DO + "|" + exports.ELIF + ")";
